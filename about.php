32f81180a8cd9783ef385a38995f140718d3176c
Fernando Fagonde <fernandofagonde@gmail.com>
2019-09-12 10:59:09 -0300

    Update about.php
    Investigação->inovação

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
                 <h2>Perfil</h2>
                 <p id="about-text">
                 A ípsillon é uma startup que visa o desenvolvimento de soluções inovadoras através de pesquisa e
-                investigação, sendo elas do eixo tecnológico ou não, baseadas nas necessidades das pessoas e em
+                inovação, sendo elas do eixo tecnológico ou não, baseadas nas necessidades das pessoas e em
                 processos produtivos e operacionais, independente dos atores e do ecossistema de atuação.
                 </p>
                 <p id="about-text">
